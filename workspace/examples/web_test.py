eb5338df960b9e7dbafcda30b3f487ce268dd3e0
Krzysztof Jamroz <kjamroz83@gmail.com>
2025-01-26 10:26:54 +0100

    add remote control to web test

--- workspace/examples/web_test.py
+++ workspace/examples/web_test.py
@@ -1,18 +1,50 @@
 #!/usr/bin/env python3
-from codecruiser import WebApp
+from codecruiser import WebApp, Motors
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from time import sleep
 
 app = WebApp()
+motors = Motors()
 
 @app.get("/")
 def read_root():
     return FileResponse("web_test.html")
 
+@app.get("/api/forward")
+def reverse_string():
+    motors.left_speed = 1
+    motors.right_speed = 1
+    sleep(0.5)
+    motors.left_speed = 0
+    motors.right_speed = 0
+    return {"status": "ok"}
 
-@app.get("/api/reverse")
-def reverse_string(text):
-    return {"reversed": text[::-1]}
-    
+@app.get("/api/backward")
+def reverse_string():
+    motors.left_speed = -1
+    motors.right_speed = -1
+    sleep(0.5)
+    motors.left_speed = 0
+    motors.right_speed = 0
+    return {"status": "ok"}
+
+@app.get("/api/left")
+def reverse_string():
+    motors.left_speed = -1
+    motors.right_speed = 1
+    sleep(0.5)
+    motors.left_speed = 0
+    motors.right_speed = 0
+    return {"status": "ok"}
+
+@app.get("/api/right")
+def reverse_string():
+    motors.left_speed = 1
+    motors.right_speed = -1
+    sleep(0.5)
+    motors.left_speed = 0
+    motors.right_speed = 0
+    return {"status": "ok"}
 
 app.start_server()
